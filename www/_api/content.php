b2372b886dffba190e0a7904ca97c2f1e6d9da29
Jose F Flores <jose.flores.152@gmail.com>
2014-07-07 16:18:31 -0400

    restructured api preprocessing results

--- www/_api/content.php
+++ www/_api/content.php
@@ -1,13 +1,9 @@
 <?php
 	/**
 	 *  @File			content.php
-	 * 	@Authors		Jose Flores
-	 * 					jose.flores.152@gmail.com
+	 * 	@Author			Jose Flores <jose.flores.152@gmail.com>
 	 * 	
-	 * 	@Description	This file holds the ajax processing catch
-	 * 
-	 * 	@changelog		
-	 *	4/20/14			Moved the catch to content file of that _api
+	 * 	This file holds the ajax processing catch
 	 */
 	 
 	//	Define guard prevents acces unless from the index.php file at 
@@ -49,23 +45,37 @@
 	 * ]
 	 * 
 	 */	
-	 
+	
+	//Sets up JSON return type 
 	header( 'Content-Type: application/json' ) ;
+	
+	// Detects if not in ssl prepares error
 	if ( $_SERVER['SERVER_NAME'] == $A[ 'PROD_DOMAIN' ] && $_SERVER[ 'SERVER_PORT' ] != 443  ){
 		$json = array( array( 'order' => 0 , 'call' => 'ssl' , 'parameter' => array( null ) ) ) ;
 	}
+	
+	// Allowed in ssl
 	else {
+		
+		// empty query response
 		$json = array( array( 'order' => 0 , 'call' => 'getMethodList' , 'parameter' => array( null ) ) ) ;
 		
-		//  JSON API Processing
-		if ( isset( $_POST[ 'JSON' ] ) &&
-			 $_POST[ 'JSON' ] != null )
-				$json = $_POST[ 'JSON' ] ;
-		//  JSON API Processing
-		else if ( isset( $_GET[ 'JSON' ] ) &&
-				  $_GET[ 'JSON' ] != null ) 
-					$json = $_GET[ 'JSON' ] ;
+		// non empty query default response
+		if ( count( $_POST ) || count( $_GET ) ) {
+			$json = array( array( 'order' => 0 , 'call' => 'isBadSyntax' , 'parameter' => array( null ) ) ) ;
+			
+			//  JSON API POST Processing
+			if ( isset( $_POST[ 'JSON' ] ) &&
+				 $_POST[ 'JSON' ] != null )
+					$json = $_POST[ 'JSON' ] ;
+					
+			//  JSON API GET Processing
+			else if ( isset( $_GET[ 'JSON' ] ) &&
+					  $_GET[ 'JSON' ] != null ) 
+						$json = $_GET[ 'JSON' ] ;
+		}
 	}
+	// process json 
 	echo trim( processJson( $A , $json ) ) ;
 	
 ?>
