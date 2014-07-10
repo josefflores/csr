f379b8f165bdeb07006f0e24f8cef53c2fa2ea9a
Sudhir Vaidya <sudhir_vaidya@student.uml.edu>
2014-07-10 16:56:44 -0400

    Revert "Tab view for profiles"
    This reverts commit fb6cc70a47a7a456820508258d8e63ea9de3222c.

--- php/wgt/profile-generator/src/profile.php
+++ php/wgt/profile-generator/src/profile.php
@@ -1,194 +1,94 @@
 
 	<div class='profile-generator'>
-        <div id="tabs" class='container1'>
-            <ul>
-                <li><a href="#tabs-1">Patient - 1</a></li>
-                <li><a href="#tabs-2">Patient - 2</a></li>
-            </ul>
-            <div id="tabs-1" class='panel'>
-                <div class='profile-generator-left'>
-
-                    <div class='profile-generator-id'>
-
-                        <div class='profile-generator-picture'>
-                            <image src='profile-generator-pic' />
-                        </div>
-
-                        <div class='profile-generator-name-card1'>
-                            <table>
-
-                                <tr>
-                                    <th>Name</th>
-                                    <td>value</td>
-                                </tr>
-
-                                <tr>
-                                    <th>Age</th>
-                                    <td>value</td>
-                                </tr>
-
-                                <tr>
-                                    <th>Height</th>
-                                    <td>value</td>
-                                </tr>
-
-                                <tr>
-                                    <th>BMI</th>
-                                    <td>value</td>
-                                </tr>
-
-                                <tr>
-                                    <th>Activity Level</th>
-                                    <td>value</td>
-                                </tr>
-
-                                <tr>
-                                    <th>Email</th>
-                                    <td>value</td>
-                                </tr>
-
-                                <tr>
-                                    <th>Alarm Number</th>
-                                    <td>value</td>
-                                </tr>
-
-                            </table>
-                        </div>
-
-                    </div>
-
-                    <!--div class='profile-generator-input'>
-                        <h3>Log</h3>
-                        <textarea class='profile-generator-input' ></textarea>
-                        <button style='float:right;'>Post</button>
-                    </div-->
-
-                    <div class='profile-generator-log'>
-
-                        <div class='profile-generator-log-entry'>
-                        </div>
-
-                        <div class='profile-generator-log-entry'>
-                        </div>
-
-                        <div class='profile-generator-log-entry'>
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div class='profile-generator-center'></div>
-
-                <div class='profile-generator-right'>
-
-                    <div class='profile-generator-graph'>
-                        Graph
-                    </div>
-
-                    <div class='profile-generator-graph'>
-                        Graph
-                    </div>
-
-                    <div class='profile-generator-graph'>
-                        Graph
-                    </div>
-
-                </div>
-            </div>
-
-
-            <div id="tabs-2" class='panel'>
-                <div class='profile-generator-left'>
-
-                    <div class='profile-generator-id'>
-
-                        <div class='profile-generator-picture'>
-                            <image src='profile-generator-pic' />
-                        </div>
-
-                        <div class='profile-generator-name-card1'>
-                            <table>
-
-                                <tr>
-                                    <th>Name</th>
-                                    <td>value</td>
-                                </tr>
-
-                                <tr>
-                                    <th>Age</th>
-                                    <td>value</td>
-                                </tr>
-
-                                <tr>
-                                    <th>Height</th>
-                                    <td>value</td>
-                                </tr>
-
-                                <tr>
-                                    <th>BMI</th>
-                                    <td>value</td>
-                                </tr>
-
-                                <tr>
-                                    <th>Activity Level</th>
-                                    <td>value</td>
-                                </tr>
-
-                                <tr>
-                                    <th>Email</th>
-                                    <td>value</td>
-                                </tr>
-
-                                <tr>
-                                    <th>Alarm Number</th>
-                                    <td>value</td>
-                                </tr>
-
-                            </table>
-                        </div>
-
-                    </div>
-
-                    <!--div class='profile-generator-input'>
-                        <h3>Log</h3>
-                        <textarea class='profile-generator-input' ></textarea>
-                        <button style='float:right;'>Post</button>
-                    </div-->
-
-                    <div class='profile-generator-log'>
-
-                        <div class='profile-generator-log-entry'>
-                        </div>
-
-                        <div class='profile-generator-log-entry'>
-                        </div>
-
-                        <div class='profile-generator-log-entry'>
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div class='profile-generator-center'></div>
-
-                <div class='profile-generator-right'>
-
-                    <div class='profile-generator-graph'>
-                        Graph
-                    </div>
-
-                    <div class='profile-generator-graph'>
-                        Graph
-                    </div>
-
-                    <div class='profile-generator-graph'>
-                        Graph
-                    </div>
-
-                </div>
-            </div>
-        </div>
 	
+		<div class='profile-generator-left'>
+		
+			<div class='profile-generator-id'>
+			
+				<div class='profile-generator-picture'>
+					<image src='profile-generator-pic' />
+				</div>
+				
+				<div class='profile-generator-name-card'>
+					<table>
+					
+						<tr>
+							<th>Name</th>
+							<td>value</td>
+						</tr>
+						
+						<tr>
+							<th>Age</th>
+							<td>value</td>
+						</tr>
+						
+						<tr>
+							<th>Height</th>
+							<td>value</td>
+						</tr>
+						
+						<tr>
+							<th>BMI</th>
+							<td>value</td>
+						</tr>
+						
+						<tr>
+							<th>Activity Level</th>
+							<td>value</td>
+						</tr>
+						
+						<tr>
+							<th>Email</th>
+							<td>value</td>
+						</tr>
+						
+						<tr>
+							<th>Alarm Number</th>
+							<td>value</td>
+						</tr>
+						
+					</table>
+				</div>
+				
+			</div>
+			
+			<div class='profile-generator-input'>
+				<h3>Log</h3>
+				<textarea class='profile-generator-input' ></textarea>
+				<button style='float:right;'>Post</button>
+			</div>
+			
+			<div class='profile-generator-log'>
+			
+				<div class='profile-generator-log-entry'>
+				</div>
+				
+				<div class='profile-generator-log-entry'>
+				</div>
+				
+				<div class='profile-generator-log-entry'>
+				</div>
+				
+			</div>
+		
+		</div>
+			
+		<div class='profile-generator-center'></div>
 
+		<div class='profile-generator-right'>
+		
+			<div class='profile-generator-graph'>
+			Graph
+			</div>
+			
+			<div class='profile-generator-graph'>
+			Graph
+			</div>
+			
+			<div class='profile-generator-graph'>
+			Graph
+			</div>
+			
+		</div>	
+	</div>
+	
