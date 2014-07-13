8dccc663777528c80415e76034e44566d823ecc5
Jose F Flores <jose.flores.152@gmail.com>
2014-07-13 19:52:47 -0400

    Added Meta data and icons

--- php/head/meta.php
+++ php/head/meta.php
@@ -1,14 +1,25 @@
 <?php
 	/**
-	 *  @File			meta.php
-	 * 	@Authors		Jose Flores
-	 * 					jose.flores.152@gmail.com
+	 *  @file		meta.php
+	 * 	@author		Jose F Flores <jose.flores.152@gmail.com>
 	 * 	
-	 * 	@Description	This holds the meta information
-	 * 
-	 * 	@changelog		
-	 * 	2/20/14			Created the file
+	 * 	This holds the meta information
 	 */
 	
 	
+	echo '<meta http-equiv="content-type" content="text/html;charset=utf-8">' ;
+	echo '<meta http-equiv="content-language" content="en">' ;
+	echo '<meta http-equiv="content-language" content="en-us">' ;
+
+	echo '<meta name="keywords" content="University,  Massachusetts, MA,  Lowell, UMASS, Computer Science, CS, Department, CSR, Computational, Sensor, Research, Project, Dietary, Dietician "> <!--no longer used by Google-->' ;
+	echo '<meta name="description" content="The University of Masssachussetss Lowel CSR (Computational Sensor Research) project website, Directed by Dr. Yu Cao ">' ;
+
+	echo '<link rel="icon" href="' . $A[ 'W_IMG' ] . 'logo/icon_32.png" type="image/png">' ;
+	echo '<link rel="shortcut icon" href="' . $A[ 'W_IMG' ] . 'logo/icon_32.png" type="image/png">' ;
+
+	echo '<meta name="author" content="Jose F Flores">' ;
+	echo '<meta name="host" content="csr.cs.uml.edu">' ;
+	echo '<meta name="generator" content="Geany IDE">' ;
+	
+	echo '<title>CSR Project</title>';
 ?>
