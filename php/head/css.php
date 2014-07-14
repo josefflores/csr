485e4df4ff888b2ae2d4b21e7813da8efa289bef
Jose F Flores <jose.flores.152@gmail.com>
2014-07-14 07:13:21 -0400

    Gui changes - Changed jQuery UI theme

--- php/head/css.php
+++ php/head/css.php
@@ -14,7 +14,7 @@
 	
 	//	Downloaded css files, place here first so that the main and 
 	//	widget files can override 
-	echo '<link href="' , $A['W_CSS'] , 'jquery-ui-1.10.4.custom.css" 	rel="stylesheet" type="text/css">' ;
+	echo '<link href="' , $A['W_CSS'] , 'jquery-ui.css" 	rel="stylesheet" type="text/css">' ;
 	
 	// 	The main css file this holds most styling elements to lend 
 	//	consistency to the application
